51602f29e6dddd3620c5157d23b69a6a729f5b64
Kim Vo <126284919+KimVVo@users.noreply.github.com>
2024-04-21 12:42:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+var button = document.getElementById('myButton');
+
+// Add an event listener for the click event
+button.addEventListener('click', function() {
+    // Show a message when the button is clicked
+    alert('Button was clicked!');
+});
